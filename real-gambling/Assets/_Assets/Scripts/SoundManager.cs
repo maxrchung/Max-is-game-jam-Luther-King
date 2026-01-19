d67bd8cbfd4da76d01b8bbf9ed8ee935f4cfc20f
WL <alakamande@gmail.com>
2026-01-18 21:04:02 -0800

    reel icons, sounds, sound manager

---
+++ real-gambling/Assets/_Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioClip[] media;
+    public AudioSource source;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayIntroAndLoop();
+    }
+
+    public void PlaySound(int sound)
+    {
+        source.PlayOneShot(media[sound]);
+    }
+
+    public void PlayIntroAndLoop()
+    {
+        float delayLength = media[0].length;
+        source.PlayOneShot(media[0]);
+        source.clip = media[1];
+        source.loop = true;
+        source.PlayDelayed(delayLength);
+    }
+
+    public void StopSong()
+    {
+        source.Stop();
+    }
+}
